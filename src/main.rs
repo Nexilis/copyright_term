679a90b6d7e126be3d6104d2c337a79967a39a4c
Bartek Łukasik <Nexilis@users.noreply.github.com>
2019-12-29 11:03:58 +0100

    Printing with breaks simplification
    Ownership improvements

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ fn main() {
         .read_line(&mut user_input)
         .expect("Error reading line");
 
-    let x1: Vec<&str> = user_input.split(';').collect();
+    let user_input_split: Vec<&str> = user_input.split(';').collect();
 
-    let days_with_splits: Vec<i32> = x1
+    let periods: Vec<usize> = user_input_split
         .iter()
         .map(|x| x.trim().parse().expect("Wrong input"))
         .collect();
 
     let mut index = 0;
-    let working_days: i32 = days_with_splits.iter().fold(0, |acc, x| {
+    let working_days: usize = periods.iter().fold(0, |acc, x| {
         if index % 2 == 0 {
             index += 1;
             acc + *x
@@ -31,7 +31,10 @@ fn main() {
     let not_copyrighted = working_hours - copyrighted;
     let copyright_ratio = copyrighted / working_hours;
 
-    println!("Sum of: working days: {}, working hours: {}", working_days, working_hours);
+    println!(
+        "Sum of: working days: {}, working hours: {}",
+        working_days, working_hours
+    );
 
     println!(
         "Hours copyrighted: {}, not copyrighted: {}, ratio: {}\n",
@@ -40,12 +43,12 @@ fn main() {
 
     let calendar = gen_calendar(working_days as usize);
 
-    print(&calendar, &days_with_splits);
+    print(&calendar, &periods);
 }
 
-fn gen_calendar(days: usize) -> Vec<i32> {
+fn gen_calendar(days: usize) -> Vec<usize> {
     let hours = (days * 8) as f32;
-    let mut remaining = (hours * 0.8).floor() as i32;
+    let mut remaining = (hours * 0.8).floor() as usize;
     let mut calendar = vec![0; days];
     while remaining > 0 {
         let index = rand::thread_rng().gen_range(0, days);
@@ -65,29 +68,24 @@ fn gen_calendar(days: usize) -> Vec<i32> {
     return calendar;
 }
 
-fn print(calendar: &Vec<i32>, breaks: &Vec<i32>) {
+fn print(calendar: &Vec<usize>, breaks: &Vec<usize>) {
     println!("\nCopyright hours:");
-    print_with_breaks(&calendar, &breaks);
+    print_with_breaks(calendar, breaks);
 
     println!("\nRemaining hours:");
-    let remaining: Vec<i32> = calendar.iter().map(|x| 8 - *x).collect();
-    print_with_breaks(&remaining, &breaks);
+    let remaining: Vec<usize> = calendar.iter().map(|x| 8 - *x).collect();
+    print_with_breaks(&remaining, breaks);
 }
 
-fn print_with_breaks(calendar: &Vec<i32>, breaks: &Vec<i32>) {
-    let mut index = 0;
-    let mut presented = 0;
-    for pair in breaks.iter().map(|x| {
-        index += 1;
-        (index, *x)
-    }) {
-        let (i, val) = pair;
+fn print_with_breaks(calendar: &Vec<usize>, breaks: &Vec<usize>) {
+    let mut printed_work_days = 0;
 
-        if i % 2 != 0 {
-            for x in calendar.iter().skip(presented).take(val as usize) {
+    for (i, &val) in breaks.iter().enumerate() {
+        if i % 2 == 0 {
+            for x in calendar.iter().skip(printed_work_days).take(val) {
                 println!("{}", *x);
             }
-            presented += val as usize;
+            printed_work_days += val;
         } else {
             for _ in 0..val {
                 println!(" ");
@@ -105,7 +103,7 @@ mod tests {
         let days = 0..101;
         for d in days {
             println!("Testing days: {}", d);
-            let expected: i32 = (d as f32 * 8. * 0.8).floor() as i32;
+            let expected = (d as f32 * 8. * 0.8).floor() as usize;
 
             let actual = gen_calendar(d).iter().sum();
             assert_eq!(expected, actual);
